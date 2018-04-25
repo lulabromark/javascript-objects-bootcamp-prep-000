bb219499dac96d55aa7782ac2f43a5902c69347f
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-25 00:51:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: 'songTitle'}
 
-function updatePlayList(playlist, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   playist.artistName = 'songTitle'
   return playist
 }
